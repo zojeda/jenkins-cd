497d8446566da75f4fabe1ae25842dadc2aa7918
Zacarias F. Ojeda <zojeda@hexacta.COM>
2016-08-04 20:43:33 +0100

    updating plugins

--- listPlugins.sh
+++ listPlugins.sh
@@ -2,5 +2,5 @@
 
 #read plugin list from a working jenkins with CloudBees+Docker+Pipeline+Plugin -> 
 #https://github.com/jenkinsci/docker-workflow-plugin/blob/master/demo/README.md
-curl 'http://localhost:8080/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins' | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g' > plugins.txt
+curl 'http://<user>:<api_token>@<jenkins_host>:<jenkins_port>/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins' | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g' > plugins.txt
 
